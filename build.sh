dd7d4c673b07c2378672b41ed51881353f4999af
phone-burner <51866924+phone-burner@users.noreply.github.com>
2021-06-13 15:12:26 +0800

    Update build.sh
    增加自动构建 `bam-0.4.0` 的功能，免去自备 `bam` 的麻烦

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 # bam need be compiled
 
 BASEDIR=$(dirname "$0")
-cd $BASEDIR && docker build -t mmotee_build . && docker run --rm --name mmotee_build -v $PWD:/mmotee_cn -v $(realpath $1):/bam --privileged mmotee_build sh -c "cd /mmotee_cn && ../bam/bam config && ../bam/bam server_sql_release && ../bam/bam server_sql_debug"
+cd $BASEDIR && docker build -t mmotee_build . && docker run --rm --name mmotee_build -v $PWD:/mmotee_cn --privileged mmotee_build sh -c "apt install wget unzip -y && wget https://github.com/matricks/bam/archive/refs/tags/v0.4.0.zip && unzip v0.4.0.zip && cd bam-0.4.0 && ./make_unix.sh&&cd /mmotee_cn && /bam-0.4.0/bam config && /bam-0.4.0/bam server_sql_release && /bam-0.4.0/bam server_sql_debug"
