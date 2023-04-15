628955f43f8dc9bd2b9c8d611ac853f162596605
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2023-04-15 19:19:39 +0000

    臭一下看历史Commit

--- src/game/version.h
+++ src/game/version.h
@@ -8,6 +8,6 @@
 #define GAME_NETVERSION "0.6" //GAME_NETVERSION_HASH
 // 下面的这行,只要加入就报错,而且也没引用,干脆删了得了
 // FFS注：我修了，但是确实没用，所以还是不要了吧（）
-// static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '.', '4', 0};
-// static const char GAME_RELEASE_VERSION[8] = "0.6.4";
+// static const char GAME_RELEASE114514_VERSION[8] = {'0', '.', '6', '.', '4', 0};
+// static const char GAME_RELEASE1919810_VERSION = "0.6.4";
 #endif
